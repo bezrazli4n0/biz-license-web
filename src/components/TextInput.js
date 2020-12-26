8e54622069fbe5a6f8c914a932a7c0178fbc92cc
bezrazli4n0 <rpglovemmo@gmail.com>
2020-12-26 15:30:46 +0300

    Fixed undefined behavior when trying to change key input.

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function TextInput(params) {
+    console.log(params.onChange);
     let isPassword = false;
+    const isCallback = params.onChange !== undefined;
 
     if (params.type !== undefined)
         if (params.type === "password")
@@ -11,7 +13,7 @@ function TextInput(params) {
         <div className="pt-3">
             <label>{params.label}</label>
             <div className="input-group">
-                {isPassword ? <input type="password" className="form-control" onChange={ (event) => params.onChange(event.target.value) } value={params.text} /> : <input type="text" className="form-control" onChange={ (event) => params.onChange(event.target.value) } value={params.text} />}
+                {isPassword ? <input type="password" className="form-control" onChange={ (event) => isCallback ? params.onChange(event.target.value) : null } value={params.text} /> : <input type="text" className="form-control" onChange={ (event) => isCallback ? params.onChange(event.target.value) : null } value={params.text} />}
             </div>
         </div>
     );
